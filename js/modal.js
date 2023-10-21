97321ef34663f95df27dae1092aa58a4702ff80e
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-10-22 01:57:08 +0300

    half star

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,23 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+const getted=2.75;
+
+const modalRatingValue=document.querySelector(".modal-rating-value");
+ modalRatingValue.textContent=Math.round(getted *10)/10;
+ const modalRating = document.querySelector(".modal-rating");
+const ratingValue = modalRating.querySelector(".modal-rating-value").textContent;
+
+// Визначаємо кількість зірок, які потрібно відобразити
+const numberOfStars = Math.floor(ratingValue);
+const fractionStar = getted % 1;
+console.log(fractionStar);
+// Відображуємо зірки
+const starring = modalRating.querySelector(".starring");
+for (let i = 0; i < numberOfStars; i++) {
+  const star = starring.querySelector(`.modal-rating-star[data-value="${i + 1}"]`);
+  star.querySelector(".modal-icon-star").classList.remove("inactive");
+  star.querySelector(".modal-icon-star").classList.add("modal-icon-star");}
+  star=starring.querySelector(`.modal-rating-star[data-value="${numberOfStars + 1}"]`);
+  fractedStar=star.querySelector(".modal-icon-star");
+fractedStar.style.fill = "linear-gradient(to right, rgba(238, 161, 12, 1) 30%, rgba(244, 244, 244, 0.2) 70%)"
