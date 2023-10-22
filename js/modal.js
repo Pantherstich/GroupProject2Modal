e9f4f6e07ff32884709c3a4c9c9325a1bbb0d466
Pantherstich <pantherstich@gmail.com>
2023-10-22 11:27:56 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -20,6 +20,26 @@
 })();
 const getted=4.75;
 
+// "_id": "64f389465ae26083f39b17a4",
+// "bodyPart": "waist",
+// "equipment": "body weight",
+// "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0003.gif",
+// "name": "air bike",
+// "target": "abs",
+// "description": "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+// "rating": 4.64,
+// "burnedCalories": 312,
+// "time": 3,
+// "popularity": 529
+
+
+
+
+
+
+
+
+
 const modalRatingValue=document.querySelector(".modal-rating-value");
  modalRatingValue.textContent=Math.round(getted *10)/10;
  const modalRating = document.querySelector(".modal-rating");
